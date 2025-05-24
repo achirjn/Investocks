79013eaa1d77181a0c79b5a22d71fd0a442c52a4
Achir Jain <achirjain11@gmail.com>
2025-05-24 14:05:11 +0530

    stage 1 updates: basic trade working without portfolio

---
+++ InveStocks/src/test/java/com/investocks/InveStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.investocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InveStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
